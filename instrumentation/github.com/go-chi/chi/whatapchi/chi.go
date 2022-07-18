d7cf2f6bd9e80183c7cbbf89e8066248364ae266
DESKTOP-QCGUFT0\hamma <jhlim@whatap.io>
2022-07-18 18:35:58 +0900

    feat: go-chi middleware 수정

--- instrumentation/github.com/go-chi/chi/whatapchi/chi.go
+++ instrumentation/github.com/go-chi/chi/whatapchi/chi.go
@@ -3,24 +3,11 @@ package whatapchi
 import (
 	"net/http"
 
-	"github.com/whatap/go-api/config"
 	"github.com/whatap/go-api/trace"
 )
 
 func Middleware(next http.Handler) http.Handler {
-
-	return func(next http.Handler) http.Handler {
-		conf := config.GetConfig()
-		if !conf.TransactionEnabled {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				next.ServeHTTP(w, r)
-			})
-		} else {
-			return trace.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				next.ServeHTTP(w, r)
-			})
-		}
-
-	}(next)
-
+	return trace.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+	})
 }
