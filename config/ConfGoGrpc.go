6f747baac96afead62cc2d42acbe5ab972f2e6d9
DESKTOP-QCGUFT0\hamma <jhlim@whatap.io>
2022-07-25 13:34:44 +0900

    feat: config 수정 (grps 관련)

--- config/ConfGoGrpc.go
+++ config/ConfGoGrpc.go
@@ -20,8 +20,8 @@ func (this *ConfGoGrpc) ApplyDefault(m map[string]string) {
 }
 func (this *ConfGoGrpc) Apply(conf *Config) {
 	this.GoGrpcProfileEnabled = conf.Enabled && conf.GetBoolean("go.grpc_profile_enabled", true)
-	this.GoGrpcProfileStreamClientEnabled = conf.GoGrpcProfileEnabled && conf.GetBoolean("go.grpc_profile_stream_client_enabled", true)
-	this.GoGrpcProfileStreamServerEnabled = conf.GoGrpcProfileEnabled && conf.GetBoolean("go.grpc_profile_stream_server_enabled", true)
+	this.GoGrpcProfileStreamClientEnabled = this.GoGrpcProfileEnabled && conf.GetBoolean("go.grpc_profile_stream_client_enabled", true)
+	this.GoGrpcProfileStreamServerEnabled = this.GoGrpcProfileEnabled && conf.GetBoolean("go.grpc_profile_stream_server_enabled", true)
 	this.GoGrpcProfileIgnoreMethod = conf.GetStringArray("go.grpc_profile_ignore_method", ",")
 	this.GoGrpcProfileStreamMethod = conf.GetStringArray("go.grpc_profile_stream_method", ",")
 	this.GoGrpcProfileStreamIdentify = this.GoGrpcProfileEnabled && conf.GetBoolean("go.grpc_profile_stream_identify", false)
